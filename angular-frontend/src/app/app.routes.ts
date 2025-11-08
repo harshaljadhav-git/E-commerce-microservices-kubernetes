3d6ddda922381e4bce27b02f2c4e0ae253b60c5a
harshalj257 <harshalj257@gmail.com>
2025-11-08 18:56:41 +0000

    Docker update

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
 import { OrderComponent } from './order/order.component';
+import { HomeComponent } from './home/home.component'; // Import HomeComponent
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Set HomeComponent as the default route
   { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductComponent },
+  { path: 'product', component: ProductComponent }, // Corrected path
   { path: 'orders', component: OrderComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
